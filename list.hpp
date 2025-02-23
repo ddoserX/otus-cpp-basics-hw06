73781957f55866d474d15b5b860400c6e42537c0
Trofim <dualdual657@gmail.com>
2025-02-24 01:09:22 +1000

    list.hpp early

--- list.hpp
+++ list.hpp
@@ -3,9 +3,19 @@
 namespace Container
 {
     template <typename T>
+    struct Node
+    {
+        Node *previous;
+        Node *next;
+        T data;
+    };
+
+    template <typename T>
     class List : public IContainer<T>
     {
     public:
+        List();
+
         void push_back(const T &value) override;
         void insert(const size_t &pos, const T &value) override;
         void erase(const size_t &pos) override;
@@ -15,6 +25,18 @@ namespace Container
 
     private:
         size_t m_size;
-        T *m_array;
+        Node<T> m_node;
+        Node<T> *m_previous;
+        Node<T> *m_next;
     };
+
+    template <typename T>
+    inline List<T>::List() : m_size{0}, m_node{}, m_next{nullptr}, m_previous{nullptr}
+    {
+    }
+
+    template <typename T>
+    inline void List<T>::push_back(const T &value)
+    {
+    }
 } // namespace Container
